399bc5cb747e45232bd7802b90ccf0a7c7750e98
Abzed Mohammed <ibraabzed@gmail.com>
2022-07-14 13:55:56 +0300

    changed side icons

--- my_profile/static/js/index.js
+++ my_profile/static/js/index.js
@@ -1,9 +1,20 @@
-// function wrapAllFunctions(){
-//     setTimeout(greetings(), 1000)
-// }
-// wrapAllFunctions()
+var i = 0;
+var images = [];
+var slideTime = 3000; // 3 seconds
 
-// function greetings(){
-//     const body = document.querySelector('body')
-//     body.innerHTML = "Hello and welcome..."
-// }
+images[0] = '../image/burn.jpg';
+images[1] = '../image/vivid.jpg';
+images[2] = '../image/punch.jpg';
+
+function changePicture() {
+    document.body.style.backgroundImage = "url(" + images[i] + ")";
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(changePicture, slideTime);
+}
+
+window.onload = changePicture;
