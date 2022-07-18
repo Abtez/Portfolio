f8f34764c1940858e926ed4d3c04be92aca226ed
Abzed Mohammed <ibraabzed@gmail.com>
2022-07-18 18:28:51 +0300

    pushing to heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,17 +28,6 @@ TAGGIT_CASE_INSENSITIVE = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 cloudinary.config(
     cloud_name = os.getenv('CLOUD_NAME'),
