74492b14a7d70b7519445b9ef191527f3d64c5cb
Abzed Mohammed <ibraabzed@gmail.com>
2022-07-31 15:40:28 +0300

    fix static4

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'my_profile/static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
